686aaa3e1654398ca44fa0ba8fcd262272e19957
luzz <874536791@qq.com>
2020-07-20 17:44:03 +0800

    no message

--- packages/flv/src/index.vue
+++ packages/flv/src/index.vue
@@ -81,7 +81,7 @@ export default {
     <path fill="#ffffff" d="M43.935,25.145c0-10.318-8.364-18.683-18.683-18.683c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615c8.072,0,14.615,6.543,14.615,14.615H43.935z" transform="rotate(275.098 25 25)">
         <animateTransform attributeType="xml" attributeName="transform" type="rotate" from="0 25 25" to="360 25 25" dur="0.6s" repeatCount="indefinite"></animateTransform>
     </path>
-</svg><p>加载中...</p></div>`,
+</svg><p class="loading_title">加载中...</p></div>`,
       type: String,
     },
   },
@@ -218,7 +218,9 @@ video {
   bottom: 0;
   z-index: 1000;
 }
-p {
+.loading_title {
   font-size: 14px;
+  height: 20px;
+  line-height: 20px;
 }
 </style>
